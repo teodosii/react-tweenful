ee0c4c163de8125740b17144881a0c4955bd7d8e
Rares Mardare <teodosii3@outlook.com>
2020-03-11 15:33:54 +0200

    Added copy 404 to webpack config, updated routes for gh pages

--- site/components/App.js
+++ site/components/App.js
@@ -15,17 +15,17 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/react-tweenful">
           <Header />
         </Route>
-        <Route path="/notifications" component={NotificationsDemo} />
-        <Route path="/transition" component={RouteTransitionDemo} />
-        <Route path="/observer" component={ObserverDemo} />
-        <Route path="/svg" component={SvgDemo} />
-        <Route path="/loading-circles" component={LoadingCircles} />
-        <Route path="/rotating-svg" component={RotatingSvg} />
-        <Route path="/gradients" component={Gradients} />
-        <Route path="/bouncing-balls" component={BouncingBalls} />
+        <Route path="*/notifications" component={NotificationsDemo} />
+        <Route path="*/transition" component={RouteTransitionDemo} />
+        <Route path="*/observer" component={ObserverDemo} />
+        <Route path="*/svg" component={SvgDemo} />
+        <Route path="*/loading-circles" component={LoadingCircles} />
+        <Route path="*/rotating-svg" component={RotatingSvg} />
+        <Route path="*/gradients" component={Gradients} />
+        <Route path="*/bouncing-balls" component={BouncingBalls} />
       </Switch>
     </Router>
   );
